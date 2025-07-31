fff85dfbf0acfb214b337e1116f95aa2df235a56
hyomin <dksgyals1@gmail.com>
2025-07-31 10:03:23 +0900

    [refactor] format code

--- test_calc.py
+++ test_calc.py
@@ -1,12 +1,14 @@
 import pytest
 from calc import Calc
 
+
 def test_calc():
-    calc = Calc()
-    assert calc is not None
+    assert 1 == 1
+    pytest.fail()
+
 
 def test_get_minus():
-    cases = [(4,3,1), (3,4,-1), (10,10,0)]
+    cases = [(4, 3, 1), (3, 4, -1), (10, 10, 0)]
     calc = Calc()
 
     for case in cases:
