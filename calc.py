cc0505a3b36ef0653dd1ea330d009aa7bcb7e190
donghee <do.choi@samsung.com>
2025-07-31 09:55:52 +0900

    feature-getgop

--- calc.py
+++ calc.py
@@ -1,3 +1,6 @@
 class Calc:
     # 이곳에 코드를 작성
     pass
+
+    def getGop(self, a, b):
+        return 6
--- test_calc.py
+++ test_calc.py
@@ -1,5 +1,12 @@
 import pytest
 
+from calc import Calc
+
+
 def test_calc():
     assert 1==1
     pytest.fai()
+
+def test_getgop():
+    calc = Calc()
+    assert calc.getGop(2,3)  == 6
