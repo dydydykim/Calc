7e44181b27aab477f7a75e2811613feaee4685dc
dykim <dykim4225@gmail.com>
2025-07-31 09:53:26 +0900

    [feature] Add getZegop
    Signed-off-by: dykim <dykim4225@gmail.com>

--- calc.py
+++ calc.py
@@ -1,3 +1,5 @@
 class Calc:
     # 이곳에 코드를 작성
+    def getZegop(self, a):
+        return a*a
     pass
--- test_calc.py
+++ test_calc.py
@@ -1,5 +1,16 @@
 import pytest
+from calc import Calc
+
 
 def test_calc():
     assert 1==1
     pytest.fai()
+
+def test_getZegop():
+    calc= Calc()
+    assert calc.getZegop(1) == 1
+    assert calc.getZegop(2) == 4
+    assert calc.getZegop(3) == 9
+    assert calc.getZegop(-1) == 1
+    assert calc.getZegop(-2) == 4
+
