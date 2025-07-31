2891c6a0b21f5c614220061dbe5422a7963c9f63
hyomin <dksgyals1@gmail.com>
2025-07-31 09:52:17 +0900

    [feature] get_minus initial

--- calc.py
+++ calc.py
@@ -1,3 +1,6 @@
 class Calc:
     # 이곳에 코드를 작성
     pass
+
+    def get_minus(self, param, param1):
+        return 1
--- test_calc.py
+++ test_calc.py
@@ -1,5 +1,11 @@
 import pytest
+from calc import Calc
 
 def test_calc():
-    assert 1==1
-    pytest.fai()
+    calc = Calc()
+    assert calc is not None
+
+def test_get_minus():
+    calc = Calc()
+    ret = calc.get_minus(4, 3)
+    assert ret == 1
