6b78c714cb1070e7040d5a1db460e95e6c5e0d7a
alvin1785 <alvin1785@gmail.com>
2025-07-31 10:02:38 +0900

    [refactor] getDivide 함수 일반화 및 예외처리

--- calc.py
+++ calc.py
@@ -1,3 +1,13 @@
 class Calc:
     # 이곳에 코드를 작성
     pass
+
+    def getDivde(self, first_num, second_num):
+        try:
+            result = first_num / second_num
+        except ZeroDivisionError:
+            return "오류: 0으로 나눌 수 없습니다."
+        except TypeError:
+            return "오류: 숫자만 입력해야 합니다."
+        else:
+            return result
--- test_calc.py
+++ test_calc.py
@@ -1,6 +1,13 @@
 import pytest
 
+from calc import Calc
+
+
 def test_calc():
-    assert 1==1
+    assert 1 == 1
     # pytest.fail()
 
+
+def test_get_divide():
+    calc = Calc()
+    assert calc.getDivde(5,1) == 5
