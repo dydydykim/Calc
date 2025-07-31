5c7cff69f09ca898a03cb9adadc3e706d8bd7f1a
dhoon21 <ijdmc2002@naver.com>
2025-07-31 10:08:00 +0900

    [feature] get sum
    Signed-off-by: dhoon21 <ijdmc2002@naver.com>

--- calc.py
+++ calc.py
@@ -2,4 +2,5 @@ class Calc:
     # 이곳에 코드를 작성
     def getZegop(self, a):
         return a*a
-    pass
+    def getSum(self, a, b):
+        return a+b
--- test_calc.py
+++ test_calc.py
@@ -14,3 +14,11 @@ def test_getZegop():
     assert calc.getZegop(-1) == 1
     assert calc.getZegop(-2) == 4
 
+def test_getSum():
+    calc = Calc()
+    assert calc.getSum(1, 2) == 3
+    assert calc.getSum(1, 0) == 1
+    assert calc.getSum(0, 0) == 0
+    assert calc.getSum(-1, 1) == 0
+    assert calc.getSum(-1, 0) == -1
+    assert calc.getSum(-1, -2) == -3
